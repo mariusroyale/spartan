e76f3eadae25513078a8482155ff556d447bd991
Marius Iordache <marius.technical.leader@gmail.com>
2025-08-01 01:04:37 -0400

    Add initial main script

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use clap::Parser;
+use fs_extra::file::{copy, CopyOptions};
+use log::{error, info};
+use std::fs;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
+
+/// Simple file sync utility
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    // Source directory to sync from
+    #[arg(short, long)]
+    source: PathBuf,
+
+    /// Destination directory (e.g., your 24TB HDD mount)
+    #[arg(short, long)]
+    destination: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let source = Path::new(&args.source);
+    // let destination = Path::new(&args.destination);
+
+    if !source.exists() || !source.is_dir() {
+        anyhow::bail!("Source path doesn't exist or is not a directory");
+    }
+
+    // lets print what is in the source directory
+    for entry in WalkDir::new(source) {
+        let entry = entry?;
+        let path = entry.path();
+        println!("{}", path.display());
+    }
+
+    Ok(())
+}
